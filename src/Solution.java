41ae1babe0b9379faef737bce9e7e77d8a287397
Aditya Singh Chauhan <aditya.chuahan_cs18@gla.ac.in>
2020-05-06 09:50:32 +0530

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        Character[] S= {'h', 'e', 'l', 'l', 'o'};
+        int index = 0;
+        char req = S[index];
+        for (char c : str.toCharArray())
+        {
+            if (c == req && index == 4)
+            {
+                System.out.println("YES");
+                return;
+            }
+            else if (c == req)
+            {
+                index++;
+                req = S[index];
+            }
+        }
+        System.out.println("NO");
+    }
+}
