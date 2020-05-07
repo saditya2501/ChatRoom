4eb954cbfb4d9a3168a81bcf0ac83e7aa12541fb
Aditya Singh Chauhan <aditya.chuahan_cs18@gla.ac.in>
2020-05-07 09:14:52 +0530

    Initial Commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,7 @@
 //HackerRank Q Solution
 import java.util.Scanner;
 public class Solution {
-
+//
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
