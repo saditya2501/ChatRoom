1757bd896330919f52d2c40734f21af4676e134c
Aditya Singh Chauhan <aditya.chuahan_cs18@gla.ac.in>
2020-05-08 22:12:21 +0530

    Initial Commit

--- src/Solution.java
+++ src/Solution.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 public class Solution {
     //
-    public static void main(String[] args) {
+    public static void main(String[] args) {//
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
